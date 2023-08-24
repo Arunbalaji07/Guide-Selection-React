b7dfe4559b9fa166d1020d15808ffb2b71f8e30e
Arunbalaji07 <stararunbalaji12@gmail.com>
2023-08-24 21:06:50 +0530

    Added navbar to the website

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes ,Route} from "react-router-dom";
+import './App.css';
+
+import Home from './pages/Home';
+import GuideList from "./pages/GuideList";
+import Admin from './pages/Online';
+import Staff from './pages/Staff';
+import Student from "./pages/Student";
+
+import Navbarmenu from './Components/Navbarmenu'
+
+function App() {
+  return (
+    <div>
+      <Router basename="/">
+
+        {/* Add Menu Component */}
+        <Navbarmenu />
+
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/guide-list" element={<GuideList />}/>
+          <Route path="/admin" element={<Admin />}/>
+          <Route path="/staff" element={<Staff />}/>
+          <Route path="/student" element={<Student />}/>
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Student.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+    <div className="sec__one">
+      <h1> Student </h1>
+    </div>
+  )
+}
+export default Student
