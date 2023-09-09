b01394781a2000c6e971df98069eb73aabb6df2d
Abhi-lash27 <babhilash27@gmail.com>
2023-09-09 13:13:19 +0530

    Added Login pages for Admin,Staff and Student

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import '../pages/css/Login.css';
+import user from '../img/user.png';
+import password from '../img/password.png';
 
 const Staff = () => {
   return (
     <div className="sec__one">
-      <h1> Staff </h1>
+      <div className="form">
+        <div className="header">
+          <div className="text">Signin</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <img src={user} alt="" />
+            <input type="email" placeholder="Email Address" required />
+          </div>
+          <div className="input">
+            <img src={password} alt="" />
+            <input type="password" placeholder="Password" required />
+          </div>
+        </div>
+        <div className="submit-cont">
+          <div className="submit">Login</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import '../pages/css/Login.css';
+import user from '../img/user.png';
+import password from '../img/password.png';
 
 const Student = () => {
   return (
     <div className="sec__one">
-      <h1> Student </h1>
+      <div className="form">
+      <div className="header">
+        <div className="text">Login</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user} alt="" />
+          <input type="text" placeholder="Register Number" required />
+        </div>
+        <div className="input">
+          <img src={password} alt="" />
+          <input type="password" placeholder="Password" required/>
+        </div>
+      </div>
+      <div className="forgot-pass">Forgot Password?<span>Click Here</span></div>
+      <div className="submit-cont">
+        <div className="submit">Login</div>
+      </div>
+      </div>
     </div>
   );
 };
