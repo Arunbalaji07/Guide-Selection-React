6441cc91f7e28a6ecc334cd6bac85e502580a154
Arunbalaji07 <stararunbalaji12@gmail.com>
2023-10-05 14:29:54 +0530

    updated StudentDashboard for dynamic

--- src/scences/dashboard/StudentDashboard.jsx
+++ src/scences/dashboard/StudentDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { ColorModeContext, useMode } from "../../theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import "../../index.css";
@@ -7,6 +7,14 @@ import StudentSidebar from "../global/StudentSidebar";
 
 const StudentDashboard = () => {
   const [theme, colorMode] = useMode();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:7777/api/projects`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
@@ -19,42 +27,105 @@ const StudentDashboard = () => {
             <div>
               <h2>Student Information</h2>
               <table>
-              <tr>
-                  <th>Project Title</th>
-                  <td>Cell 1</td>
-                </tr>
-                <tr>
-                  <th>Team Member 1</th>
-                  <td>Cell 1</td>
-                </tr>
-                <tr>
-                  <th>Register No 1</th>
-                  <td>Cell 2</td>
-                </tr>
-                <tr>
-                  <th>Guide Name</th>
-                  <td>Cell 2</td>
-                </tr>
-                <tr>
-                  <th>Email 1</th>
-                  <td>Cell 2</td>
-                </tr>
-                <tr>
-                  <th>Phone No 1</th>
-                  <td>Cell 2</td>
-                </tr>
-                <tr>
-                  <th>Team Member 2</th>
-                  <td>Cell 2</td>
-                </tr>
-                <tr>
-                  <th>Register No 2</th>
-                  <td>Cell 2</td>
-                </tr>
-                <tr>
-                  <th>Email 2</th>
-                  <td>Cell 2</td>
-                </tr>
+                <tbody>
+                  {(data.length > 0)? data.map((data, index) => {
+                        return (
+                          <tr key={index}>
+                            <tr>
+                              <th>Project Title</th>
+                              <td>{data.title}</td>
+                            </tr>
+                            <tr>
+                              <th>Team Member 1</th>
+                              <td>{data.member1}</td>
+                            </tr>
+                            <tr>
+                              <th>Register No 1</th>
+                              <td>{data.regno1}</td>
+                            </tr>
+                            <tr>
+                              <th>Guide Name</th>
+                              <td>{data.guide}</td>
+                            </tr>
+                            <tr>
+                              <th>Email 1</th>
+                              <td>{data.email1}</td>
+                            </tr>
+                            <tr>
+                              <th>Phone No 1</th>
+                              <td>{data.phno1}</td>
+                            </tr>
+                            <tr>
+                              <th>Team Member 2</th>
+                              <td>{data.member2}</td>
+                            </tr>
+                            <tr>
+                              <th>Register No 2</th>
+                              <td>{data.regno2}</td>
+                            </tr>
+                            <tr>
+                              <th>Email 2</th>
+                              <td>{data.email2}</td>
+                            </tr>
+                          </tr>
+                        );
+                      })
+                    : null }
+                  {/* : //{" "} */}
+                  {/* <tr>
+                    // <th>Project Title</th>
+                    // <td>Cell 1</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Team Member 1</th>
+                    // <td>Cell 1</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Register No 1</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Guide Name</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Email 1</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Phone No 1</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Team Member 2</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Register No 2</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr>
+                  //{" "}
+                  <tr>
+                    // <th>Email 2</th>
+                    // <td>Cell 2</td>
+                    //{" "}
+                  </tr> */}
+                </tbody>
               </table>
             </div>
           </main>
